3983d8fc1c41b0c496b5e84f64c72f2d7a9693c1
dtalochk <denis.talochkin@gmail.com>
2021-06-19 00:09:58 +0300

    Remove stupid redirect from portal.host.it to www.portal.host.it

---
+++ src/CarLicensePlatesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class CarLicensePlatesList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+
+        return (
+            <ul>
+                <li>Ferrari</li>
+                <li>Lada</li>
+                <li>Toyota</li>
+            </ul>
+        );
+    }
+}
+
+export default CarLicensePlatesList;
